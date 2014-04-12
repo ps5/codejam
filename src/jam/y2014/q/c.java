728ff46a39f9df2363177e3af2e3080678b44d32
PaulS <va3paw@gmail.com>
2014-04-12 14:03:46 -0400

    C - new rectangle3 method

--- src/jam/y2014/q/c.java
+++ src/jam/y2014/q/c.java
@@ -24,6 +24,13 @@ public class c {
     static int rectX = 0;
     static int rectY = 0;
 
+    static int rectX1 = 0;
+    static int rectY1 = 0;
+    static int rectX2 = 0;
+    static int rectY2 = 0;
+    static int rectX3 = 0;
+    static int rectY3 = 0;
+
     public static void main(String[] args) throws FileNotFoundException {
 
         System.out.println(filepath);
@@ -58,23 +65,30 @@ public class c {
 
         rectX = 0;
         rectY = 0; // reset
+                rectX1 = 0;
+        rectY1 = 0;
+        rectX2 = 0;
+        rectY2 = 0;
+        rectX3 = 0;
+        rectY3 = 0;
+        
         int S = C * R - M; // count of spaces not occupied by mines
 
         // if 1 free space only then win
         if (S == 1) {
-            rectX = 1;
-            rectY = 1;
+            rectX1 = 1;
+            rectY1 = 1;
             return true;
         }
 
         // if 1 row or 1 column then 1 click will win
         if (S > 0 && (C == 1 || R == 1)) {
-            rectX = C;
-            rectY = R;
+            rectX1 = C;
+            rectY1 = R;
             if (C == 1) {
-                rectY = S;
+                rectY1 = S;
             } else {
-                rectX = S;
+                rectX1 = S;
             }
             return true;
         }
@@ -86,40 +100,98 @@ public class c {
          }*/
 
         // a simple rectangle/square general case: find 2 integer divisors x, y where (x <= R && y <= C) || (..)
-        if (testRectangle(S, R, C)) {
+        if (testRectangle3(S, R, C)) {
             return true;
         }
 
- 
+        /* superseded by testRectangle3
         // test advanced rectangle/stakan subcase where it's limited by R or C (stakan)
         // as long as we get at least 2 rows or 2 columns and it's not to narrow (>2 cells wide) 
         // and it's all squares we win
         // 1. solve for y = R - fill left to the right:
-        if (S / R >= 2 && R > 2 
-                ) {
-            if ( S - (S/R)*R != 1) { // can fit a square
-            rectX = S / R;
-            rectY = R;
-            return true; }
+        if (S / R >= 2 && R > 2) {
+            if (S - (S / R) * R != 1) { // can fit a square
+                rectX = S / R;
+                rectY = R;
+                return true;
+            }
         }
         // 2. solve for x = C  - fill up to bottom:
         if (S / C >= 2 && C > 2) {
-            if (S - (S/C)*C != 1) {
-            rectX = C;
-            rectY = S / C;
-            return true;}
+            if (S - (S / C) * C != 1) {
+                rectX = C;
+                rectY = S / C;
+                return true;
+            }
         }
+*/
+        
 
         /* not one click solution?
-        // a rectange/square + 1 subcase 
-        if (testRectangle(S - 1, R - 1, C - 1)) {
-            return true;
+         // a rectange/square + 1 subcase 
+         if (testRectangle(S - 1, R - 1, C - 1)) {
+         return true;
+         }
+         */
+        return false;
+    }
+
+    public static boolean testRectangle3(int S, int R, int C) {
+
+        for (int ir = 2; ir <= R; ir++) {
+            for (int ic = 2; ic <= C; ic++) {
+                int testS = ir * ic;
+                if (testS == S) // can fit in one rectangle
+                {
+                    rectX1 = ic;
+                    rectY1 = ir;
+                    return true;
+                } else if (testS > S) {
+                    break; // skip iteration
+                } else // still fits; try a second rectangle
+                {
+                    for (int ir2 = ir + 1; ir2 <= R; ir2++) {
+                        for (int ic2 = 2; ic2 < ic; ic2++) {
+                            int testS2 = (ir2 - ir) * ic2;
+                            if (testS + testS2 == S) // 2 rectangles fit
+                            {
+                                rectX1 = ic;
+                                rectY1 = ir;
+                                rectX2 = ic2;
+                                rectY2 = ir2;
+                                return true;
+                            } else if (testS + testS2 > S) {
+                                break; // skip iteration
+                            } else // still has space; try 3rd rectangle
+                            {
+                                for (int ir3 = ir + 1; ir3 < ir2; ir3++) {
+                                    for (int ic3 = ic2 + 1; ic3 < ic; ic3++) {
+                                        int testS3 = (ic3 - ic2) * (ir3 - ir);
+                                        if (testS + testS2 + testS3 == S) // 3 rectangles fit
+                                        {
+                                            rectX1 = ic;
+                                            rectY1 = ir;
+                                            rectX2 = ic2;
+                                            rectY2 = ir2;
+                                            rectX3 = ic3;
+                                            rectY3 = ir3;
+                                            return true;
+                                        } else if (testS + testS2 + testS3 > S) {
+                                            break; // out of free space
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-                */
 
         return false;
     }
 
+    /*
     public static boolean testRectangle(int S, int R, int C) {
 
         int x = S / 2;
@@ -131,12 +203,12 @@ public class c {
                     && (x > 0 && ((x <= R && y <= C) || (x <= C && y <= R)))) // and it fits the RC boundaries
             {
                 if ((x <= R && y <= C)) {
-                rectX = y;
-                rectY = x;}
-                else
-                {
-                rectX = x;
-                rectY = y;}    
+                    rectX = y;
+                    rectY = x;
+                } else {
+                    rectX = x;
+                    rectY = y;
+                }
                 return true;
             } // win
             else {
@@ -170,7 +242,10 @@ public class c {
                     else if (S - C * (ir - 1) - ic >= 0) {
                         row += ".";
                     } else {
-                        { row += "*"; testM--; }
+                        {
+                            row += "*";
+                            testM--;
+                        }
                     }
 
                 }
@@ -188,7 +263,10 @@ public class c {
                     else if (S - R * (ic - 1) - ir >= 0) {
                         row += ".";
                     } else {
-                        { row += "*"; testM--; }
+                        {
+                            row += "*";
+                            testM--;
+                        }
                     }
 
                 }
@@ -211,7 +289,10 @@ public class c {
                             && (ir == rectY + 1 && ic == rectX + 1)) {
                         row += ".";
                     } else {
-                        { row += "*"; testM--; }
+                        {
+                            row += "*";
+                            testM--;
+                        }
                     }
                 }
                 System.out.println(row);
@@ -220,9 +301,55 @@ public class c {
             }
 
         }
-        
-        // assert(testM == 0); // make sure all mines have been displayed 
-        if (testM != 0)  throw new RuntimeException("Display error: not all mines displayed"); 
-    }
 
+        // assert(testM == 0); // make sure all mines have been displayed 
+        if (testM != 0) {
+            throw new RuntimeException("Display error: not all mines displayed");
+        }
+    }
+*/
+    
+    
+    public static void display(int R, int C, int M) {
+
+        int S = C * R - M; // count of spaces not occupied by mines\
+        int testM = M;
+
+        if (rectX1 == 0 || rectY1 == 0) {
+            throw new RuntimeException("Program error: rectX = " + rectX1 + "; rectY = " + rectY1);
+        }
+        
+        for (int ir = 1; ir <= R; ir++) {
+                String row = "";
+                for (int ic = 1; ic <= C; ic++) {
+                    if (ir == 1 && ic == 1) {
+                        row += "c";
+                    } // else if (ir <= rectY && ic <= rectX) { row += ".";
+                    else 
+                        if ((ir <= rectY1 && ic <= rectX1)  // first rectangle
+                        
+                                )        
+                        {
+                        row += ".";
+                    } else if (ir > rectY1 && ir <= rectY2 && ic <= rectX2 ) // second rectangle
+                        row += ".";
+                    else if (ir > rectY1 && ir <= rectY3  && ic > rectX2 && ic <= rectX3) // 3rd triangle
+                        row += ".";
+                        else {
+                        {
+                            row += "*";
+                            testM--;
+                        }
+                    }
+
+                }
+                System.out.println(row);
+                out.println(row);
+            }
+
+        // assert(testM == 0); // make sure all mines have been displayed 
+        if (testM != 0) {
+            throw new RuntimeException("Display error: not all mines are displayed: " + R + " " + C + " " + M);
+        }
+    }
 }
